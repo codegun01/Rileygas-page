ad9464813d22e756e99ed3d86e597e94de17445a
gonza <gonzaloriveros01@gmail.com>
2023-09-24 11:39:26 -0300

    Se agregan las animaciones

---
+++ js/anime.js
@@ -0,0 +1,76 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.titulo__banner', {
+        duration: 4000,
+        origin: 'right',
+        distance: '-250px'
+    });
+
+
+    sr.reveal('.icon__banner', {
+        duration: 4000,
+        origin: 'left',
+        distance: '-250px'
+    });
+
+
+    sr.reveal('.subtitulo__banner', {
+        duration: 4000,
+        origin: 'top',
+        distance: '-250px'
+    });
+
+
+    
+
+
+    sr.reveal('.icon__banner', {
+        duration: 4500,
+        rotate:{
+            x: 1,
+            y: 180
+        }
+    });
+
+
+    sr.reveal('.section__1', {
+        duration: 4000,
+        origin: 'top',
+        distance: '-50px'
+    });
+
+    sr.reveal('.container__mision', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-50px'
+    });
+
+    
+    sr.reveal('.container__logo2', {
+        duration: 4000,
+        origin: 'top',
+        distance: '-50px'
+    });
+
+    sr.reveal('.card', {
+        duration: 4000,
+        origin: 'top',
+        distance: '-50px'
+    });
+
+    sr.reveal('.footer__card1', {
+        duration: 4000,
+        origin: 'top',
+        distance: '-50px'
+    });
+
+
+    /*======= instalaciones ========= */
+
+
+
+    sr.reveal('.card__insta', {
+        duration: 4000,
+        origin: 'top',
+        distance: '-50px'
+    });
